b29ccc591d55e610830b5fb41a762921ebb9b90f
Taijaun Pitt <taijaun@hotmail.co.uk>
2025-04-06 23:54:04 +0100

    Refactored functions out of main.py

---
+++ stats.py
@@ -0,0 +1,20 @@
+def get_book_text(filepath):
+    with open(filepath) as f:
+        file_contents = f.read()
+    
+    return file_contents
+
+def words_in_string(text):
+    count = 0
+    words = text.split()
+
+    for word in words:
+        count += 1
+
+    return count
+
+def print_words_found():
+    book_string = get_book_text("books/frankenstein.txt")
+    word_count = words_in_string(book_string)
+
+    print(f"{word_count} words found in the document")
